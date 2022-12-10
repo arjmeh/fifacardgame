2039565599deae71c9352377ef4b90546dac7152
arjmeh <arjunisawesome101@gmail.com>
2022-12-10 15:06:20 -0800

    backend

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click', () => {
+toggleButton.addEventListener('click', async () => {
     navbarLinks.classList.toggle('active')
+    const returnValue = await axios.get('http://127.0.0.1:5000/getData');
+    console.log(returnValue.data);
 })
