6dd8fb8e5840b102ebcddbeee6e61ee10a29a950
arjmeh <arjunisawesome101@gmail.com>
2022-12-11 08:08:26 -0800

    fetch

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-// import axios from 'axios';
+//import axios from 'axios';
+
+// npx parcel index.html to run local server
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', async () => {
     navbarLinks.classList.toggle('active')
-    //const returnValue = await axios.get('http://127.0.0.1:5000/getData');
-    //console.log(returnValue.data);
-})
+    // const returnValue = await axios.get('http://127.0.0.1:5000/getData');
+    // console.log(returnValue.data);
+    // doPostRequest('testing')
+    fetch('http://127.0.0.1:5000/post', {
+        method: 'POST',
+        body: JSON.stringify('hi'
+        ),
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+    });
+
+    fetch('http://127.0.0.1:5000/get')
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+    });
+
+
+    })
+
+
+
+
+
+
+
+
+
+ 
