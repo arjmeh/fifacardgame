1819772348681dc822440dbe1a47a0641a1152d0
arjmeh <arjunisawesome101@gmail.com>
2022-12-23 12:20:47 -0800

    prettier

--- login.js
+++ login.js
@@ -2,6 +2,26 @@ var username = document.getElementById('username-input');
 var password = document.getElementById('password-input');
 
 
+//cookies
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
 
 
 
@@ -20,8 +40,8 @@ function submit() {
     if (responsedata.hasOwnProperty(usernamevalue)) {
         let usernamejson = responsedata[usernamevalue]
         if(usernamejson.usernamevalue === usernamevalue && usernamejson.passwordvalue === passwordvalue) {
+        setCookie('logged_in', true, 0);
         window.location.href = "index.html";
-        setCookie('loggedIn', 'true', 0);
         }
         else {
         alert('Wrong username or password.')
--- script.js
+++ script.js
@@ -6,37 +6,37 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', async () => {
     navbarLinks.classList.toggle('active')
-    // const returnValue = await axios.get('http://127.0.0.1:5000/getData');
-    // console.log(returnValue.data);
-    // doPostRequest('testing')
 
-//     fetch('http://127.0.0.1:5000/post', {
-//     method: 'POST',
-//     headers: {
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': '*'
-//     },
-//     body: JSON.stringify({
-//     "ani": 'cataa'
-//     })
-// })
-//   .then((postresponse) => postresponse.json())
-//   .then((postdata) => console.log(postdata))
-//   .catch((posterror) => console.error(posterror));
-
-
-    fetch('http://127.0.0.1:5000/get')
-    .then(response => response.text())
-    .then(data => {
-    let responsedata = data;
-    console.log(responsedata);
-    })
-    .catch((error) => console.error(error));
 })
     //data is the get data
 
 
 //cookies
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+
+
+
+
+
 //leaderboard
 fetch('http://127.0.0.1:5000/get')
     .then(response => response.text())
@@ -93,6 +93,7 @@ for (const user of userArray.slice(0,10)) {
   rank++;
 }
 table.style.fontSize = '2rem';
+table.style.padding = '2rem';
 // Step 5: find the "grid-rankings" div
 const rankingsDiv = document.querySelector('.grid-rankings');
 
@@ -100,10 +101,6 @@ const rankingsDiv = document.querySelector('.grid-rankings');
 rankingsDiv.appendChild(table);
     })
     .catch((error) => console.error(error));
-
-
-
-
 
 
 
